a8536bd21cf5a84b6bec5ad297a4f89515781096
calvarycat <ngoctrungbmt@gmail.com>
2018-08-24 13:55:15 +0700

    InitProject

---
+++ Assets/Quizz.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class Quizz  {
+
+    public string keyword;
+    public List<string> hints;
+    public int startFrom;
+    public int keyhightlight;
+
+}
---
+++ Assets/UIQuiz.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIQuiz : MonoBehaviour {
+    public static UIQuiz instance;
+    public List<Quizz> listQuiz;
+    public GameObject PrefabsQuiz;
+    public Transform RootQuiz;
+    // Use this for initialization
+    private void Awake()
+    {
+        instance = this;
+    }
+    void Start () {
+        InitQuizUI();
+
+    }
+    public void OnShow(bool isShow)
+    {
+        RootQuiz.gameObject.SetActive(isShow);
+
+    }
+	
+	public void InitQuizUI()
+    {
+        foreach(Quizz q in listQuiz)
+        {
+            GameObject obj = Instantiate(PrefabsQuiz, RootQuiz);
+            QuizzItem qI = obj.GetComponent<QuizzItem>();
+            qI.Init(q, OnQuestionClick);
+            //obj.Init();
+        }
+    }
+    public void OnQuestionClick(Quizz q)
+    {
+
+        OnShow(false);
+        QuizzDetail.instance.OnInitQuiz(q);
+        Debug.Log("qiz" + q.keyword);
+    }
+}
